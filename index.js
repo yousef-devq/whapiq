8b81d4c85294a46affc282abdd41cef9891603a0
yousef-devq <yousefjaser@gmail.com>
2025-04-28 22:48:46 +0300

    Railway4

--- index.js
+++ index.js
@@ -189,7 +189,6 @@ app.post('/check-number', async (req, res) => {
 });
 
 // تشغيل الخادم
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
